0d86f1fa06e4c8e25d3195493af37e7691bb10bc
teraria-m-ito <m-ito@teraria-labs.jp>
2026-09-08 03:04:53 +0900

    通常ログイン時もcurrent_lms_userを参照できるように変更

--- app/controllers/rbase_lti_module/admin_users/sessions_controller_ext.rb
+++ app/controllers/rbase_lti_module/admin_users/sessions_controller_ext.rb
@@ -4,9 +4,7 @@ module RbaseLtiModule
       include ::Lti::OperationLogCreation
 
       def after_sign_in_path_for_with_rbase_lti_module(resource)
-        if current_admin_user && session[:current_lms_user].blank?
-          session[:current_lms_user] = ::LmsUser.where(admin_user_id: current_admin_user.id).first
-        end
+        ensure_current_lms_user_session
         create_view_operation(:common, nil, I18n.t(:"views.lti.common.operation_log.sign_in.title"))
         after_sign_in_path_for_without_rbase_lti_module(resource)
       end
@@ -15,6 +13,15 @@ module RbaseLtiModule
         create_view_operation(:common, nil, I18n.t(:"views.lti.common.operation_log.sign_out.title"))
         destroy_without_rbase_lti_module
         session[:current_lms_user] = nil
+      end
+
+      private
+
+      def ensure_current_lms_user_session
+        return unless current_admin_user
+        return if session[:current_lms_user].present?
+
+        session[:current_lms_user] = ::LmsUser.where(admin_user_id: current_admin_user.id).first
       end
     end
   end
